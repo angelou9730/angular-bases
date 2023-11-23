1ff3d232ab56cb2e931c2b704def90f1c4d0bf6d
Angelo Miranda <angelojesusmirandazapata@hotmail.com>
2023-11-22 23:04:12 -0500

    fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template:`
+  <p>Counter: {{counter}}</p>
+  <button (click)="increaseBy(1)">+1</button>
+  <button (click)="reset()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+  public counter:number =10;
+
+  increaseBy(value:number):void{
+    this.counter+=value;
+  }
+  reset(){
+    this.counter=10;
+  }
+
+
+}
